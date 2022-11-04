03b163fa3f231f4e65ac7f0eafb75141ae040b64
DJJJonas <jonasfilho73@gmail.com>
2022-11-04 20:37:52 -0300

    gitignore and gradle config

---
+++ src/main/java/com/djjjonas/pokemonapi/PokemonApiApplication.java
@@ -0,0 +1,13 @@
+package com.djjjonas.pokemonapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonApiApplication.class, args);
+	}
+
+}
